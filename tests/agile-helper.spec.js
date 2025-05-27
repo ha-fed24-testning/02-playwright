2d017b66f41145bc215c1e5588e638055572d194
David Andersson <lejonmanen@gmail.com>
2025-05-27 13:53:58 +0200

    Sprint planning är synlig

--- tests/agile-helper.spec.js
+++ tests/agile-helper.spec.js
@@ -9,4 +9,25 @@ test.describe('Agile helper', () => {
 	test('titeln "Agile helper" visas', async ({ page }) => {
 		await expect(page).toHaveTitle('Agile helper')
 	})
+
+	/*
+	1b Gör ett test för följande scenario:
+	Klicka på knappen med texten "Första"
+	Klicka på knappen som innehåller texten "Sprint planning"
+	Kontrollera att en rubrik med texten "Sprint planning" är synlig på webbsidan.
+	*/
+	test('Rubriken "Sprint planning" visas när man öppnar dialogrutan', async ({ page }) => {
+		// Alternativ 1
+		const firstButton = page.getByRole('button', { name: 'Första' })
+		await firstButton.click({ timeout: 500 })  // timeout är valfritt
+
+		// Alternativ 2
+		// await page.getByRole('button', { name: 'Första' }).click()
+
+		// Använd /../i för ett "regex" som är case insensitive
+		await page.getByRole('button', { name: 'Sprint planning' }).click({ timeout: 500 })
+
+		const heading = page.getByRole('heading', { name: 'Sprint planning' })
+		await expect(heading).toBeVisible({ timeout: 500 })
+	})
 })
